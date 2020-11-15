fedbe3594859dcbee58ff2b71851bd0cdca30295
Kirithiha <kirithiha2000@gmail.com>
2020-11-15 14:23:46 +0530

    Changes in Category and Product

---
+++ app/src/main/java/com/example/grocerydelivery/Models/LoginDetails.java
@@ -0,0 +1,32 @@
+package com.example.grocerydelivery.Models;
+
+public class LoginDetails {
+    String phno, password,role;
+
+    public LoginDetails() {
+    }
+
+    public String getPhno() {
+        return phno;
+    }
+
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = "Customer";
+    }
+}
